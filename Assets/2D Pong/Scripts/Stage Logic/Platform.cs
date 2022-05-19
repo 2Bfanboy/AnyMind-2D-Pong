148b0fb9195dd59f581fdfe423433459b32ed121
2Bfanboy <andrewCarreraJP@gmail.com>
2022-05-19 19:48:38 +0900

    Adds movement controls for demo build (android) using left/right screen touch

--- Assets/2D Pong/Scripts/Stage Logic/Platform.cs
+++ Assets/2D Pong/Scripts/Stage Logic/Platform.cs
@@ -13,20 +13,54 @@ namespace AnyMind.Pong2D
         [SerializeField]
         private Rigidbody2D rigidbodyReference;
 
-        private float input;
-
-        private void Update()
+        private enum PlatformStates
         {
-            input = Input.GetAxisRaw("Horizontal");
+            MovingLeft,
+            MovingRight,
+            NotMoving
         }
+        private PlatformStates state = PlatformStates.NotMoving;
 
         private void FixedUpdate()
         {
-            if (rigidbodyReference != null)
+            if (rigidbodyReference == null)
             {
-                // For quicker editor testing with keyboard -- release will use on-screen buttons
-                rigidbodyReference.velocity = Vector2.right * input * dragSpeed;
+                return;
+            }
+            
+            switch (state)
+            {
+                case PlatformStates.MovingLeft:
+                    rigidbodyReference.velocity = Vector2.right * -1 * dragSpeed;
+                    return;
+                case PlatformStates.MovingRight:
+                    rigidbodyReference.velocity = Vector2.right * 1 * dragSpeed;
+                    return;
+                case PlatformStates.NotMoving:
+                    rigidbodyReference.velocity = Vector2.zero;
+                    return;
             }
         }
+
+        #region Controls (via UI buttons)
+        // Note: Because this is just a demo, I'm using this simple movement controller.
+        // If the user pressed both left and right, it'll move in the direction of the most recently pressed button
+        // On release of either buttons, movement will stop. Ideally the user will only request movement in 1 direction
+        // at a time.
+        public void LeftPressed()
+        {
+            state = PlatformStates.MovingLeft;
+        }
+
+        public void RightPressed()
+        {
+            state = PlatformStates.MovingRight;
+        }
+        
+        public void RightOrLeftReleased()
+        {
+            state = PlatformStates.NotMoving;
+        }
+        #endregion
     }
 }
